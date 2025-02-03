dd264de143b4fbcdf37dac3ddcebb3011a3fe3da
LDenysiuk <liudmyla@studysmarter.de>
2025-02-04 00:29:24 +0200

    finish

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 
 import { LoginComponent } from './auth/login/login.component';
 import { LoginComponent2 } from "./auth/login-2/login.component2";
+import { SignupComponent } from "./auth/signup/signup.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [LoginComponent, LoginComponent2],
+  imports: [LoginComponent, LoginComponent2, SignupComponent],
 })
 export class AppComponent {}
